9d58e6febd82d3792c650eeaee6d456ba4b5b613
Mukul Thakur <95705739+mrthakur30@users.noreply.github.com>
2023-06-06 09:47:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           dispatch(setToken(tokenString));
           console.log(tokenString);
        }
-      // window.location.hash = "";
+       window.location.hash = "";
                                  
   },[dispatch,token]);
   
